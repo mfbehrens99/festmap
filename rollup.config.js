5ccb7b04be2f4c8c6ebdfaaa595981d0795bf90e
Michael Behrens <mfbehrens99@gmail.com>
2023-06-27 13:41:28 +0200

    Setup terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,64 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import copy from 'rollup-plugin-copy'
+import { terser } from "rollup-plugin-terser";
 
 import postcss from 'rollup-plugin-postcss';
 import postcssImport from 'postcss-import';
 import postcssCopy from 'postcss-copy';
 
+import pkg from './package.json'
+
+const production = !process.env.ROLLUP_WATCH
+const bannerContent = `
+/**
+ * ${pkg.name}
+ * ${pkg.description}
+ *
+ * @version ${pkg.version}
+ * @author ${pkg.author}
+ * @license ${pkg.license}
+ * @link ${pkg.repository.url}
+ */
+`.trimStart()
 
 module.exports = [
   {
-    input: 'src/main.js',
+    input: pkg.module,
     output: {
-      file: 'dist/festmap-src.js',
+      file: pkg.main,
       format: 'umd',
-      sourcemap: true,
+      sourcemap: production ? false : true,
+      banner: bannerContent
     },
     plugins: [
       resolve(),
       commonjs(),
       copy({
         targets: [
-          {src: 'src/index.html', dest: 'dist'},
+          { src: 'src/index.html', dest: 'dist' },
         ]
-      })
+      }),
+      production && terser({
+        mangle: true,
+        format: {
+          comments: false,
+          preamble: bannerContent,
+        }
+      }),
     ]
   },
   {
     input: 'src/styles/main.css',
     output: {
-      file: 'dist/festmap.css',
+      file: pkg.style,
       format: 'es'
     },
     plugins: [
       postcss({
         extract: true,
         inject: false,
-        minimize: true,
+        minimize: production ? true : false,
         plugins: [
           postcssImport({}),
           postcssCopy({
